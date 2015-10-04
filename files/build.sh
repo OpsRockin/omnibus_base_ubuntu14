9a8d930825ec55801e92e0a6cd52c15348bd4f82
sawanoboly <sawanoboriyu@higanworks.com>
2015-10-04 14:07:59 +0900

    clone cache before build

--- files/build.sh
+++ files/build.sh
@@ -4,4 +4,11 @@ set -ex
 source /home/omnibus/load-omnibus-toolchain.sh
 cd /home/omnibus/omnibus-project
 bundle install --binstubs bundle_bin --without development test
+
+if ! [ "$BUILD_CACHE_PATH" == "" ] ; then
+  if [ -d ${BUILD_CACHE_PATH} ] ; then
+    git clone ${BUILD_CACHE_PATH} /opt/${OMNIBUS_PROJECT}
+  fi
+fi
+
 ./bundle_bin/omnibus build ${OMNIBUS_PROJECT}
